6991ebcbf427e662a3d3610678f7b10048894349
Adler Coelho <adler.coelho.santos@icloud.com>
2021-04-27 19:29:53 -0300

    Adicionando componente para criar listagem de cards

--- lib/main.dart
+++ lib/main.dart
@@ -1,68 +1,58 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Pagellll'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'Apertou:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
+void main() => runApp(
+  MaterialApp(
+    home: Scaffold(
+      body: ListaTransferencias(),
+      appBar: AppBar(title: const Text('Transferências'),),
       floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.plus_one),
-      ),  // This trailing comma makes auto-formatting nicer for build methods.
+        child: Icon(Icons.add),
+        onPressed: null,
+      ),
+    ),
+    debugShowCheckedModeBanner: false,
+  )
+);
 
+class ListaTransferencias extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Column(
+      children: [
+        ItemTransferencia(Transferencia('100.00', '0001')),
+        ItemTransferencia(Transferencia('200.00', '0002')),
+        ItemTransferencia(Transferencia('300.00', '0003')),
+      ],
     );
   }
+}
+
+class ItemTransferencia extends StatelessWidget {
+
+  final Transferencia transferencia;
+
+
+  ItemTransferencia(this.transferencia);
+
+  @override
+  Widget build(BuildContext context) {
+
+    return Card(
+      child: ListTile(
+        leading: Icon(Icons.monetization_on),
+        title: Text(transferencia.valor.toString()),
+        subtitle: Text(transferencia.numeroConta.toString()),
+      ),
+    );
+  }
+
+}
+
+class Transferencia {
+
+  final String valor;
+  final String numeroConta;
+
+  Transferencia(this.valor, this.numeroConta);
 }
