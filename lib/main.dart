d218261b57c89e708fb03a04578126c462392cff
Adler Coelho <adler.coelho.santos@icloud.com>
2021-04-29 08:52:52 -0300

    Adicionando nova página e criando método para persistir uma transferência

--- lib/main.dart
+++ lib/main.dart
@@ -1,29 +1,95 @@
+import 'dart:ffi';
+
 import 'package:flutter/material.dart';
 
-void main() => runApp(
-  MaterialApp(
-    home: Scaffold(
-      body: ListaTransferencias(),
-      appBar: AppBar(title: const Text('Transferências'),),
-      floatingActionButton: FloatingActionButton(
-        child: Icon(Icons.add),
-        onPressed: null,
+void main() => runApp(ByteBankApp());
+
+class FormularioTransferencia extends StatelessWidget {
+
+  final TextEditingController _controladorCampoNumeroConta = TextEditingController();
+  final TextEditingController _controladorCampoValor = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Criando Transferência'),),
+        body: Column(
+          children: <Widget>[
+            Padding(
+              padding: const EdgeInsets.all(28.0),
+              child: TextField(
+                controller: _controladorCampoNumeroConta,
+                keyboardType: TextInputType.number,
+                style: TextStyle(
+                  fontSize: 24.0
+                ),
+                decoration: InputDecoration(
+                  labelText: 'Número da conta',
+                  hintText: '000'
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(28.0),
+              child: TextField(
+                controller: _controladorCampoValor,
+                keyboardType: TextInputType.number,
+                style: TextStyle(
+                    fontSize: 24.0
+                ),
+                decoration: InputDecoration(
+                  icon: Icon(Icons.monetization_on),
+                    labelText: 'Valor',
+                    hintText: '0.00'
+                ),),
+            ),
+            ElevatedButton(
+              child: Text('Confirmar',),
+              onPressed: () {
+                final int numeroConta = int.tryParse(_controladorCampoNumeroConta.text) as int;
+                final double valor = double.tryParse(_controladorCampoValor.text) as double;
+
+               final Transferencia transferenciaCriada = Transferencia(
+                    valor, numeroConta);
+
+                debugPrint('Tranferência: $transferenciaCriada');
+
+              },)
+          ],
+        ),);
+  }
+}
+
+class ByteBankApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return  MaterialApp(
+      home: Scaffold(
+        body: FormularioTransferencia(),
       ),
-    ),
-    debugShowCheckedModeBanner: false,
-  )
-);
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
+
 
 class ListaTransferencias extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    return Column(
-      children: [
-        ItemTransferencia(Transferencia('100.00', '0001')),
-        ItemTransferencia(Transferencia('200.00', '0002')),
-        ItemTransferencia(Transferencia('300.00', '0003')),
-      ],
+    return Scaffold(
+      appBar: AppBar(title: const Text('Transferências'),),
+      body: Column(
+        children: [
+          ItemTransferencia(Transferencia(100.00, 0001)),
+          ItemTransferencia(Transferencia(200.00, 0002)),
+          ItemTransferencia(Transferencia(300.00, 0003)),
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: null,
+      ),
     );
   }
 }
@@ -51,8 +117,13 @@ class ItemTransferencia extends StatelessWidget {
 
 class Transferencia {
 
-  final String valor;
-  final String numeroConta;
+  final double valor;
+  final int numeroConta;
 
   Transferencia(this.valor, this.numeroConta);
+
+  @override
+  String toString() {
+    return 'Transferencia{valor: $valor, numeroConta: $numeroConta}';
+  }
 }
